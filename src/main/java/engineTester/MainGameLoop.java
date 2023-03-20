1c839d56a5e7d59e3baf41382f911e52326f668b
MartmatiX <martin.malir@gmail.com>
2023-03-20 14:20:44 +0100

    Renderer optimization

--- src/main/java/engineTester/MainGameLoop.java
+++ src/main/java/engineTester/MainGameLoop.java
@@ -6,13 +6,13 @@ import entities.Light;
 import models.TextureModel;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
-import renderEngine.DisplayManager;
-import renderEngine.Loader;
+import renderEngine.*;
 import models.RawModel;
-import renderEngine.OBJLoader;
-import renderEngine.Renderer;
 import shaders.StaticShader;
 import textures.ModelTexture;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainGameLoop {
 
@@ -21,8 +21,6 @@ public class MainGameLoop {
         DisplayManager.createDisplay();
 
         Loader loader = new Loader();
-        StaticShader shader = new StaticShader();
-        Renderer renderer = new Renderer(shader);
 
         // stall model
         RawModel model = OBJLoader.loadObjModel("stall", loader);
@@ -39,26 +37,24 @@ public class MainGameLoop {
         Entity entity = new Entity(textureModel, new Vector3f(0, 0, -25), 0, 0, 0, 1);
         Entity dragon = new Entity(dragonTexture, new Vector3f(-7, -5, -20), 0, 50, 0, 0.5f);
 
+        List<Entity> entitiesToRender = new ArrayList<>();
+        entitiesToRender.addAll(List.of(entity, dragon));
+
         Light light = new Light(new Vector3f(0, 0, -20), new Vector3f(1, 1, 1));
 
         Camera camera = new Camera();
 
+        MasterRenderer renderer = new MasterRenderer();
+
         // game logic etc...
         while (!Display.isCloseRequested()) { // Checks whether the display is closed by user
-            entity.increaseRotation(0, 1, 0);
-            dragon.increaseRotation(0, 1, 0);
             camera.move();
-            renderer.prepare();
-            shader.start();
-            shader.loadLight(light);
-            shader.loadViewMatrix(camera);
-            renderer.render(entity, shader);
-            renderer.render(dragon, shader);
-            shader.stop();
+            entitiesToRender.forEach(renderer::processEntity);
+            renderer.render(light, camera);
             DisplayManager.updateDisplay();
         }
 
-        shader.cleanUp();
+        renderer.cleanUp();
         loader.cleanUp();
         DisplayManager.closeDisplay();
 
