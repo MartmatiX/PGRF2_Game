1eb6487240f16d91dba7eb5a4a7bc0de4b47c704
MartmatiX <martin.malir@gmail.com>
2023-04-24 16:43:35 +0200

    Tweaked hitbox

--- src/main/java/collision/CollisionDetection.java
+++ src/main/java/collision/CollisionDetection.java
@@ -8,8 +8,8 @@ public class CollisionDetection {
     public static boolean detectCollision(Entity entity1, Entity entity2) {
         Vector3f position1 = entity1.getPosition();
         Vector3f position2 = entity2.getPosition();
-        float radius1 = entity1.getScale() + 2;
-        float radius2 = entity2.getScale() + 2;
+        float radius1 = entity1.getScale() + 4;
+        float radius2 = entity2.getScale() + 4;
 
         float dx = position1.x - position2.x;
         float dy = position1.y - position2.y;
